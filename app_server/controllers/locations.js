b14c4fbff7145142ee01f9d763a2966503e372d2
Ramiro Alvaro <ramiroalvaro.ra@gmail.com>
2017-04-06 13:04:26 -0300

    Creando un mixin, colocando variables en los templates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,7 +1,27 @@
 /* GET home page */
 
 module.exports.homelist = function(req, res, next){
-  res.render('locations-list', { title: 'Home'});
+  res.render('locations-list', { 
+  	title: 'Loc8r - find a place to work with wifi',
+  	pageHeader: {
+  		title: 'Loc8r',
+  		strapline: 'Find places to work with wifi near you!'
+  	},
+  	sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+  	locations: [{
+  		name: 'Starcups',
+  		address: '125 High Street, Reading, RG6 1PS',
+  		rating: 3,
+  		facilities: ['Hot drinks', 'Food', 'Premiun wifi'],
+  		distance: '100m'
+  	},{
+  		name: 'Cafe Hero',
+  		address: '120 High Street, Reading, RG6 1PS',
+  		rating: 4,
+  		facilities: ['Hot drinks', 'Premiun wifi'],
+  		distance: '200m'
+  	}]
+  });
 };
 
 /* GET Location info page */
