128cea28db47fb067aee64dcb3057bcbb0452dd1
Ramiro Alvaro <ramiroalvaro.ra@gmail.com>
2017-04-06 09:34:32 -0300

    Agregando los templates

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,17 @@
+/* GET home page */
+
+module.exports.homelist = function(req, res, next){
+  res.render('locations-list', { title: 'Home'});
+};
+
+/* GET Location info page */
+
+module.exports.locationInfo = function(req, res, next){
+  res.render('location-info', { title: 'location info'});
+};
+
+/* GET Add review page */
+
+module.exports.addReview = function(req, res, next){
+  res.render('location-review-form', { title: 'Add review'});
+};
