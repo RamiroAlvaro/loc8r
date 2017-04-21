48ff5c00ea13d0967a41e4a9e2cf52b2a28c5791
Ramiro Alvaro <ramiroalvaro.ra@gmail.com>
2017-04-21 16:13:45 -0300

    Comenzando con la SPA

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var ctrlLocations = require('../controllers/locations');
+// var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-/* Locations pages */
-router.get('/', ctrlLocations.homelist);
+
+router.get('/', ctrlOthers.angularApp);
+
+/* Locations pages comentadas las rutas cuando la aplicacion usaba express
 router.get('/location/:locationid', ctrlLocations.locationInfo);
 router.get('/location/:locationid/review/new', ctrlLocations.addReview);
 router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
 
-/* Other pages */
+Other pages
 router.get('/about', ctrlOthers.about);
+*/
 
 module.exports = router;
