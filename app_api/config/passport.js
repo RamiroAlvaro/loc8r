20f13b36722a45f3b4541965d761597815157757
Ramiro Alvaro <ramiroalvaro.ra@gmail.com>
2017-06-01 16:28:37 -0300

    Agregando manejo de usuarios

---
+++ app_api/config/passport.js
@@ -0,0 +1,12 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+    },
+    function(username, password, done) {
+        
+    }
+));
